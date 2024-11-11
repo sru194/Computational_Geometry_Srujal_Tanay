f4f4b6e18ce85eb17facae0245ea348642b02ebc
tanay-44 <136118131+tanay-44@users.noreply.github.com>
2024-11-12 03:10:38 +0530

    Update check_isomorphism.h

--- Project/check_isomorphism.h
+++ Project/check_isomorphism.h
@@ -7,9 +7,21 @@
 #include <set>
 #include <algorithm>
 
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/boyer_myrvold_planar_test.hpp>
+#include <boost/graph/is_kuratowski_subgraph.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <fstream>
+
+using namespace boost;
 using namespace std;
 
+typedef adjacency_list<vecS, vecS, undirectedS, 
+                       property<vertex_index_t, int>, 
+                       property<edge_index_t, int>> Graph;
+
 // Function declarations
-void nonIsoConnectedComponents(int vertices, const vector<pair<int, int>>& edges);
+int nonIsoConnectedComponents(int vertices, const vector<pair<int, int>>& edges);
 
 #endif
