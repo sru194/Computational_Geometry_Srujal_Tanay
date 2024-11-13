aa9577c634a357878ae65c9bf0471342c4b48606
tanay-44 <136118131+tanay-44@users.noreply.github.com>
2024-11-14 04:31:17 +0530

    Update check_isomorphism.h

--- Project/check_isomorphism.h
+++ Project/check_isomorphism.h
@@ -3,11 +3,23 @@
 
 #include <iostream>
 #include <vector>
-#include <map>
 #include <set>
+#include <unordered_map>
 #include <algorithm>
 #include <fstream>
+#include <boost/config.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/isomorphism.hpp>
+#include <boost/graph/connected_components.hpp>
+#include <boost/property_map/property_map.hpp>
+
+using namespace boost;
 using namespace std;
+
+typedef adjacency_list<vecS, vecS, undirectedS,property<vertex_index_t, int>,property<edge_index_t, int>> Graph;
+typedef graph_traits<Graph>::vertex_descriptor vertex_desc;
+typedef graph_traits<Graph>::edge_descriptor edge_desc;
 
 // Function declarations
 int nonIsoConnectedComponents(int vertices, const vector<pair<int, int>>& edges);
